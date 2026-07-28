3ba156bc8b1c5ecebae8e430985af33192f65995
tugvaselcuk <muhammedenesakdag76@gmail.com>
2026-07-28 11:39:57 +0300

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCBhz_wjBmHaMjOlJJH85JlyXxJuz74tFE",
+  authDomain: "tugva-yaz-okulu-finalleri.firebaseapp.com",
+  projectId: "tugva-yaz-okulu-finalleri",
+  storageBucket: "tugva-yaz-okulu-finalleri.firebasestorage.app",
+  messagingSenderId: "930118014370",
+  appId: "1:930118014370:web:a7350c1e44bb471a05cefb"
+};
+
+export const MAX_QUOTA = 45;
